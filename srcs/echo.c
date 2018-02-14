eaf8b50e8e0eb373f28ddf4a72f94616729a8fac
Deville Matthew <kemiimek@live.fr>
2018-02-14 19:30:54 +0100

    MA-GNI-FI-QUE

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/13 16:14:48 by mdeville          #+#    #+#             */
-/*   Updated: 2018/02/13 16:24:54 by mdeville         ###   ########.fr       */
+/*   Updated: 2018/02/14 19:29:47 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void echo(char **split)
 	size_t i;
 
 	if (!split[1])
+	{
 		ft_printf("\n");
+		return ;
+	}
 	i = 1;
 	while(split[i + 1])
 		ft_printf("%s ", split[i++]);
